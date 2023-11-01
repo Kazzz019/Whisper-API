92d946be093586ed3a5d65ad8e806740f33a5964
Kazzz019 <kazuma11b26@icloud.com>
2023-11-02 08:34:05 +1000

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,45 +51,51 @@ export default function Home() {
           setConvertedText(data.text);}
 
   return (
-            <section className="w-full vh-100 pt-10 pb-10 bg-black">
-              <div className="container px-4 md:px-6">
-                <div className="grid gap-6 items-center">
-                  <div className="flex flex-col justify-center space-y-4 text-center">
-                    <div className="space-y-2">
-                      <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
-                        Convert audio file to text
-                      </h1>
-                      <p className=" text-zinc-200 md:text-2xl dark:text-zinc-100 mx-auto">
-                        Put the audio file less than 25mb.
-                      </p>
-                    </div>
-                    <div className="w-full max-w-sm space-y-2 mx-auto">
-                      <div className="flex space-x-2" >
-                        <input
-                          className="max-w-lg flex-1 bg-gray-800 text-white border-gray-900"
-                          placeholder="Enter your audio file"
-                          type="file"  
-                          accept="audio/*" 
-                          onChange={handleFile}
-                        />
-                        <button className="bg-white text-black cursor-pointer" onClick={sendAudio}>Convert to text</button>
-                      </div>
-                      </div>
-                  </div>
-                </div>
-                </div>
-                <div className = "mx-10">
-                  <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Your text
-                  </label>
-                  <textarea
-                    id="message"
-                    className="block p-2.5 w-full text-sm h-96 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Write your thoughts here..."
-                    defaultValue={convertedText} />
-                    {loading && <p className="text-center text-white">ロード中...</p>}
-                </div>    
-                    
-              
-            </section>
+    <main className="w-full h-screen py-12 bg-black">
+    <div className="container mx-auto px-4">
+      <div className="grid gap-6 justify-center"> 
+        <div className="flex flex-col space-y-4 text-center">
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
+              Convert audio file to text
+            </h1>
+            <p className="max-w-[600px] text-zinc-200 md:text-xl dark:text-zinc-100 mx-auto">
+              Put the audio file less than 25mb.
+            </p>
+          </div>
+          <div className="w-full max-w-md space-y-2 mx-auto">
+            <div className="flex space-x-2 justify-center" >
+              <input
+                className="max-w-lg flex-1 bg-gray-800 text-white border-gray-900"
+                placeholder="Enter your audio file"
+                type="file"  
+                accept="audio/*" 
+                onChange={handleFile}
+              />
+              <button className="bg-white text-black cursor-pointer" onClick={sendAudio}>Convert to text</button>
+            </div>
+          </div>
+        </div>
+        <div className="w-full max-w-5xl mx-auto"> {/* テキストエリアの幅をさらに拡大 */}
+          <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-200 dark:text-zinc-100">
+            Transcript
+          </label>
+          <textarea
+            id="message"
+            className="block p-2.5 w-full h-[30rem] text-sm text-gray-900 bg-gray-800 rounded-lg border border-gray-700 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Text will appear here..."
+            defaultValue={convertedText}
+             />
+             {loading && <p className="text-center text-white">ロード中...</p>}
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+
+
+
+
+
            )}
